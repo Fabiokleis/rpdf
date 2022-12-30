5e513a753c123ccd1459953192f33d9f80375522
fabiokleis <fabiohkrc@gmail.com>
2022-12-30 19:13:11 -0300

    add: created a system menu to general Message actions

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -2,11 +2,11 @@ extern crate fltk;
 use fltk::{
     prelude::*,
     app::{self, Sender, Scheme}, 
-    group::Pack, menu::{Choice, MenuFlag},
-    enums::{Event, Key, Shortcut},
+    group::Pack, menu::{Choice, MenuFlag, SysMenuBar},
+    enums::{Event, Key, Shortcut, FrameType},
     window::Window,
     image::SharedImage, button,
-    dialog::{NativeFileChooser, NativeFileChooserType}
+    dialog::{self, NativeFileChooser, NativeFileChooserType, HelpDialog}
 };
 
 /// Supported actions
@@ -15,6 +15,8 @@ enum Message {
     Theme(Themes),
     FileOperation(FileOperations),
     PdfSize(PdfSizes),
+    About,
+    Help,
     Quit,
     None,
 }
@@ -83,14 +85,19 @@ pub struct RpdfApp {
     app: app::App,
     main_win: Window,
     r: app::Receiver<Message>,
+    sys_menu: MyMenu,
     imgs: Option<SharedImage>,
-    themes_dd: MyDropDownList,
-    pdf_sizes_dd: MyDropDownList,
+    pdf_sizes_dd: MyDropDownList<Choice>,
     input_btn: MyButton,
 }
 
-struct MyDropDownList {
-    dd_list: Choice,
+
+struct MyMenu {
+    menu: SysMenuBar,
+}
+
+struct MyDropDownList<M: MenuExt> {
+    dd_list: M,
 }
 
 struct MyButton {
@@ -107,16 +114,16 @@ impl MyButton {
     }
 }
 
-impl MyDropDownList {
-    fn new(label: String, choices: String, choice_message: Message, sender: &Sender<Message>) -> Self {
-        let mut dd_list = Choice::default().with_size(0, 30).with_label(label.as_str());
+impl<M: MenuExt> MyDropDownList<M> {
+    fn new(label: String, choices: String, choice_message: Message, sender: &Sender<Message>, menu: M) -> Self {
+        let mut dd_list = menu.with_size(0, 30).with_label(label.as_str());
         choices.split("|").for_each(|opt| {
             dd_list.add_choice(opt);
             let variant: Message = match choice_message {
-                Message::Theme(_) => { Themes::get_variant(opt.to_string()) },
-                Message::FileOperation(_) => { FileOperations::get_variant(opt.to_string()) },
-                Message::PdfSize(_) => { PdfSizes::get_variant(opt.to_string()) },
-                _ => { Message::None }
+                Message::Theme(_) => Themes::get_variant(opt.to_string()),
+                Message::FileOperation(_) => FileOperations::get_variant(opt.to_string()),
+                Message::PdfSize(_) => PdfSizes::get_variant(opt.to_string()),
+                _ => Message::None
             };
             dd_list.add_emit(opt, Shortcut::None, MenuFlag::Normal, sender.clone(), variant);
         });
@@ -126,36 +133,69 @@ impl MyDropDownList {
     }
 }
 
+impl MyMenu {
+    fn new() -> Self {
+        let mut menu = SysMenuBar::default().with_size(800, 35);
+        menu.set_frame(FrameType::FlatBox);
+        MyMenu {
+            menu
+        }
+    }
+
+    fn add_emit(&mut self, name: String, shortcut: Shortcut, flag: MenuFlag, sender: &Sender<Message>, message: Message) {
+        let variant: Message = match message {
+            Message::Theme(_) => {
+                let sufx = name.split("/").nth(1).unwrap();
+                Themes::get_variant(sufx.to_string())
+            },
+            Message::About => Message::About,
+            Message::Help => Message::Help,
+            Message::Quit => Message::Quit,
+            _ => Message::None,
+        };
+        self.menu.add_emit(name.as_str(), shortcut, flag, sender.clone(), variant);
+    }
+}
+
 impl RpdfApp {
     fn new() -> Self {
-        let app = app::App::default().with_scheme(Scheme::Oxy);
+        let app = app::App::default().with_scheme(Scheme::Base);
         let (s, r) = app::channel::<Message>();
         let mut main_win = Window::default()
-            .with_size(640, 480)
+            .with_size(720, 640)
             .center_screen()
             .with_label("Rpdf");
 
-        let mut pack = Pack::new(100, 45, 150, 450 - 45, "");
-        pack.set_spacing(10);
+        let mut sys_menu = MyMenu::new();
         // available themes
-        let themes_dd = MyDropDownList::new(
-            "App Theme".to_string(), 
-            "Gtk|Plastic|Gleam|Oxy".to_string(),
-            Message::Theme(Themes::Gtk),
-            &s
-        );
+        sys_menu.add_emit("&Theme/Gtk".to_string(), Shortcut::None, MenuFlag::Normal, &s, Message::Theme(Themes::Gtk));
+        sys_menu.add_emit("&Theme/Plastic".to_string(), Shortcut::None, MenuFlag::Normal, &s, Message::Theme(Themes::Plastic));
+        sys_menu.add_emit("&Theme/Gleam".to_string(), Shortcut::None, MenuFlag::Normal, &s, Message::Theme(Themes::Gleam));
+        sys_menu.add_emit("&Theme/Oxy".to_string(), Shortcut::None, MenuFlag::Normal, &s, Message::Theme(Themes::Oxy));
+
+        // actions
+        sys_menu.add_emit("&About".to_string(), Shortcut::from_char('a'), MenuFlag::Normal, &s, Message::About);
+        sys_menu.add_emit("&Help".to_string(), Shortcut::from_char('h'), MenuFlag::Normal, &s, Message::Help);
+        sys_menu.add_emit("&Quit".to_string(), Shortcut::from_char('q'), MenuFlag::Normal, &s, Message::Quit);
+
+        let mut pack = Pack::new(100, 100, 150, 450 - 45, "");
+        pack.set_spacing(10);
 
         // available pdf sizes
         let pdf_sizes_dd = MyDropDownList::new(
             "PDF size".to_string(),
             "A2|A3|A4|A5|ImgSize".to_string(),
             Message::PdfSize(PdfSizes::A4),
-            &s
+            &s,
+            Choice::default()
         );
        
         let input_btn = MyButton::new("@fileopen  Upload image".to_string(), 
             move |bt| {
                 bt.emit(s, Message::FileOperation(FileOperations::Upload));
+                let mut dialog = NativeFileChooser::new(NativeFileChooserType::BrowseMultiFile);
+                dialog.show();
+                println!("{:#?}", dialog.filenames());
             }
         );
 
@@ -170,14 +210,13 @@ impl RpdfApp {
             }
         });
 
-
         let imgs = None;
         RpdfApp {
             app,
             main_win,
             r,
+            sys_menu,
             imgs,
-            themes_dd,
             pdf_sizes_dd,
             input_btn,
         }
@@ -203,43 +242,46 @@ impl RpdfApp {
                     },
                     Message::FileOperation(fopt) => match fopt {
                         FileOperations::Upload => {
-                            println!("upload file call back!");
-                            let mut dialog = NativeFileChooser::new(NativeFileChooserType::BrowseFile);
-                            dialog.show();
-                            println!("{:#?}", dialog.filename());
+                            todo!()
                         },
                         FileOperations::Convert => {
-                            println!("convert file call back!");
+                            todo!()
                         },
                         FileOperations::Save => {
-                            println!("save file call back!");
+                            todo!()
                         },
                     },
                     Message::PdfSize(ps) => match ps {
                         PdfSizes::A2 => {
-
+                            todo!()
                         },
                         PdfSizes::A3 => {
+                            todo!()
                         },
                         PdfSizes::A4 => {
-
+                            todo!()
                         },
                         PdfSizes::A5 => {
-
+                            todo!()
                         },
                         PdfSizes::ImgSize => {
-
+                            todo!()
                         }
+                    },
+                    Message::About => {
+                        todo!()
+                    },
+                    Message::Help => {
+                        todo!()
                     },
                     Message::Quit => {
                         self.app.quit();
                     },
-                    Message::None => {}
+                    Message::None => todo!() 
                 }
             }
         }
     }
-
 }
 
 fn main() {
